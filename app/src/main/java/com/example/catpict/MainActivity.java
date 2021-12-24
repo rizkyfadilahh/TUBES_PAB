1ac67ac24d8d3e9b28c923352dc20e5cc8465e24
rizkyfadilah22 <rizkyfadilah22@gmail.com>
2021-12-24 15:53:07 +0700

    commit 2 layout done

--- app/src/main/java/com/example/catpict/MainActivity.java
+++ app/src/main/java/com/example/catpict/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.catpict;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        Adapter Adapter = new Adapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        Adapter.addFragment(new FragmentVote(), "Vote");
+        Adapter.addFragment(new FragmentBreeds(), "Breeds");
+        Adapter.addFragment(new FragmentSearch(), "Search");
+        Adapter.addFragment(new FragmentFavorites(), "Favorites");
+        viewPager.setAdapter(Adapter);
+
     }
 }
